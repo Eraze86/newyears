aa0fb30040dbf29c1020d2b274c6ee3e35916d23
Eraze86 <therese.rosvall@hotmail.se>
2021-12-28 20:35:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+////// HÄMTA /////////////////////////////////
+const userNameInput = document.getElementById("usernameinput");
+const loggInBtn = document.getElementById("logginbtn");
+const section = document.getElementById("section")
+const nav = document.getElementById("nav")
+
+///// Användare /////////////////////////////////////
+
+let users = [
+    {
+        username: "427613598",
+    }
+];
+localStorage.setItem("users", JSON.stringify(users))
+
+/////////Logga in knappen, rätt eller felanvändare////////////////////////////////////////////
+
+loggInBtn.addEventListener("click", () => {
+    let userInput = userNameInput.value
+    for (let i = 0; i < users.length; i++) {
+        if (userInput == users[i].username){
+            loggIN()
+        }
+        else {
+            let wrongUser = document.createElement("div")
+            wrongUser.innerText = "Fel kod,  försök igen";
+            document.body.append(wrongUser);
+        }
+    }
+})
+
+
+///////////Logga in sidan///////////////////////////////////////
+
+function loggIN() {
+    nav.innerHTML = `Lös pusslet ` 
+    section.innerHTML = 
+    "<div id='one'><img src='img/pussel.jpg'></div>"
+    localStorage.setItem("stay", 1)
+}
+
+///////////Kolla om man är inloggad///////////////////////////////////////
+function check() {
+    if (localStorage.getItem("stay") == 1) {
+        loggIN()
+    };
+}
+window.onload = check();
+
